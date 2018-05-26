5a90ebfdfbf232e7d95cd018af57aea4ac07702f
Fernando <fernandofleuryg@gmail.com>
2018-05-26 23:41:03 +0200

    Fix url append

--- src/setUrl.js
+++ src/setUrl.js
@@ -1,10 +1,10 @@
 import { replace } from 'ramda';
 const urlFormat = /\/\d{2}-\d{2}-\d{3}-\d{2}/;
 
-const replaceWithId = (id, path) => replace(urlFormat, id, path);
+const replaceWithId = (id, path) => path.replace(urlFormat, `/${id}`);
 
 export default id => {
-  const { origin, pathname } = window.location;
+  const { pathname } = window.location;
   const path = urlFormat.test(pathname) ? replaceWithId(id, pathname) : pathname + id;
 
   return history.pushState(null, null, path);
